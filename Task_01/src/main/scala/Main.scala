602d272dee6cb2dd1e8ae853f607b0046db0d783
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-13 17:35:02 +0200

    json parsing kinda works

--- Task_01/src/main/scala/Main.scala
+++ Task_01/src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 import scala.io.Source
 
+import spray.json._
+import DefaultJsonProtocol._
+
 object Main {
 
     final val JSON_PATH: String = "./src/data/dblp.v12.json";
@@ -12,8 +15,17 @@ object Main {
         // JSON stuff
         val fileLines = Source.fromFile(JSON_PATH).getLines();
 
+        // Skip first line, it only contains a []
+        fileLines.next();
+
         fileLines.take(500).foreach(eachLineString => {
-            println(eachLineString);
+
+            val cleanedLineString = eachLineString.replaceFirst("^,", "");
+            //            println(cleanedLineString);
+
+            val parsedJsonObject = cleanedLineString.parseJson.asJsObject;
+
+            println(parsedJsonObject.getFields("authors"));
         });
 
         DatabaseManager.closeConnection;
