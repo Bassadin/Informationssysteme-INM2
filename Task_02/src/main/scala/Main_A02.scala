6d1441d30e68a76e7d746c2b66b9c052f625d566
Bastian Hodapp <bastianhodapp@gmail.com>
2022-10-30 18:25:34 +0100

    move handlelinestring into own file

--- Task_02/src/main/scala/Main_A02.scala
+++ Task_02/src/main/scala/Main_A02.scala
@@ -1,13 +1,9 @@
 import Helpers.getCurrentTimeStringFrom
-import JsonDefinitions.Article
-import spray.json._
 import scala.io.Source
-import JsonDefinitions.ArticleProtocol._
-import java.text.SimpleDateFormat
 
 object Main_A02 {
     val JSON_PATH = "./src/data/dblp.v12.json";
-    val LOGGING_FREQUENCY_LINES = 200_000;
+    val LOGGING_FREQUENCY_LINES = 2_000;
 
     def main(args: Array[String]): Unit = {
         // Measure time before starting as reference timeframe
@@ -25,7 +21,7 @@ object Main_A02 {
 
         // Use zipWithIndex to get an index iterator alongside the elements
         jsonFileLinesIterator.zipWithIndex.foreach { case (eachLineString, eachIndex) =>
-            handleLineString(eachLineString);
+            Parsing.handleLineString(eachLineString);
 
             if (eachIndex % LOGGING_FREQUENCY_LINES == 0) {
                 Helpers.printElapsedTimeStatusMessage(eachIndex, millisecondsTimeOnStart);
@@ -40,33 +36,5 @@ object Main_A02 {
 
         println(s"Total elapsed time: ${getCurrentTimeStringFrom(millisecondsTimeOnStart)}");
         println("Terminated.");
-    }
-
-    /** Handle a json line string for insertion into redis
-      * @param eachLineString
-      *   The string to compute
-      */
-    def handleLineString(eachLineString: String): Unit = {
-        // Return for last line
-        if (eachLineString.charAt(0) == ']') {
-            return;
-        }
-
-        val cleanedLineString = eachLineString.replace("\uFFFF", "?").replaceFirst("^,", "");
-        val parsedArticle: Article = cleanedLineString.parseJson.convertTo[Article];
-
-        RedisDatabaseManager.addArticle(parsedArticle);
-
-        parsedArticle.authors match {
-            case Some(i) =>
-                RedisDatabaseManager.addAuthors(i);
-                RedisDatabaseManager.addArticleToAuthorsRelation(parsedArticle, i);
-            case None =>
-        }
-
-        parsedArticle.references match {
-            case Some(i) => RedisDatabaseManager.addArticleToArticlesRelation(parsedArticle, i);
-            case None    =>
-        }
     }
 }
---
+++ Task_02/src/main/scala/Parsing.scala
@@ -0,0 +1,35 @@
+import JsonDefinitions.Article
+import spray.json._
+import JsonDefinitions.ArticleProtocol._
+
+object Parsing {
+
+    /** Handle a json line string for insertion into redis
+      *
+      * @param eachLineString
+      *   The string to compute
+      */
+    def handleLineString(eachLineString: String): Unit = {
+        // Return for last line
+        if (eachLineString.charAt(0) == ']') {
+            return;
+        }
+
+        val cleanedLineString = eachLineString.replace("\uFFFF", "?").replaceFirst("^,", "");
+        val parsedArticle: Article = cleanedLineString.parseJson.convertTo[Article];
+
+        RedisDatabaseManager.addArticle(parsedArticle);
+
+        parsedArticle.authors match {
+            case Some(i) =>
+                RedisDatabaseManager.addAuthors(i);
+                RedisDatabaseManager.addArticleToAuthorsRelation(parsedArticle, i);
+            case None =>
+        }
+
+        parsedArticle.references match {
+            case Some(i) => RedisDatabaseManager.addArticleToArticlesRelation(parsedArticle, i);
+            case None    =>
+        }
+    }
+}
