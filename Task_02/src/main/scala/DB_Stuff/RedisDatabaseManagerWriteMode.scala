c44d5afea3f457b180e0e647d1a8d2429f1ff950
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-05 15:32:26 +0100

    add timeout of 20 secs to redis write mode manager

--- Task_02/src/main/scala/DB_Stuff/RedisDatabaseManagerWriteMode.scala
+++ Task_02/src/main/scala/DB_Stuff/RedisDatabaseManagerWriteMode.scala
@@ -1,15 +1,17 @@
 package DB_Stuff
 
 import redis.clients.jedis.args.FlushMode
-import redis.clients.jedis.{Jedis, JedisPool, Pipeline}
+import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig, Pipeline}
 
 object RedisDatabaseManagerWriteMode {
     final val DB_HOST = "localhost";
     final val DB_PORT = 32321;
+    final val JEDIS_TIMEOUT = 20 * 1000;
 
     final val PIPELINE_SYNC_LINE_FREQUENCY = 200_000;
 
-    private val jedisConnectionPool: JedisPool = new JedisPool(DB_HOST, DB_PORT);
+    private val jedisPoolConfig: JedisPoolConfig = new JedisPoolConfig();
+    private val jedisConnectionPool: JedisPool = new JedisPool(jedisPoolConfig, DB_HOST, DB_PORT, JEDIS_TIMEOUT);
     private val jedisInstance: Jedis = jedisConnectionPool.getResource;
 
     jedisInstance.flushAll(FlushMode.SYNC);
