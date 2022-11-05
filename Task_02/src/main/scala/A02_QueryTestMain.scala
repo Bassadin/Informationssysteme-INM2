da38d52fa63937eee5bbfe45ae76b3704e308885
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-05 13:21:22 +0100

    add colons to comments

--- Task_02/src/main/scala/A02_QueryTestMain.scala
+++ Task_02/src/main/scala/A02_QueryTestMain.scala
@@ -5,11 +5,11 @@ object A02_QueryTestMain {
     def main(args: Array[String]): Unit = {
         println("Starting...");
 
-        println("Title for article 1091");
+        println("Title for article 1091:");
         println(QueryManager.titleByID(1091));
-        println("Authors for article 1091");
+        println("Authors for article 1091:");
         println(QueryManager.authors(1091));
-        println("Articles for author 2312688602");
+        println("Articles for author 2312688602:");
         println(QueryManager.articles(2312688602L));
 
         println(s"Total elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
