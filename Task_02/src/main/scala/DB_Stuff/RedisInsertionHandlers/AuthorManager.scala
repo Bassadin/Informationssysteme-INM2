0844624795932cf63dbed1952bf04a039529ead7
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-05 16:20:46 +0100

    also add author ids into sets with and without hyperloglog for counting

--- Task_02/src/main/scala/DB_Stuff/RedisInsertionHandlers/AuthorManager.scala
+++ Task_02/src/main/scala/DB_Stuff/RedisInsertionHandlers/AuthorManager.scala
@@ -8,6 +8,9 @@ import spray.json.enrichAny
 object AuthorManager extends RedisManagerTrait {
     val redisPrefix = "author_"
 
+    val AUTHORS_IDS_EXACT_SET_KEY = redisPrefix + "ids_exact_set";
+    val AUTHORS_IDS_PF_SET_KEY = redisPrefix + "ids_pf_set";
+
     /** Add multiple authors to the DB
       *
       * @param authorsToAdd
@@ -15,10 +18,17 @@ object AuthorManager extends RedisManagerTrait {
       */
     def addAuthors(authorsToAdd: List[Author]): Unit = {
         authorsToAdd.foreach(eachAuthor => {
-            val redisKeyName: String = redisPrefix + eachAuthor.id;
             val authorJsonString: String = eachAuthor.toJson.compactPrint;
 
-            RedisDatabaseManagerWriteMode.jedisPipeline.set(redisKeyName, authorJsonString);
+            RedisDatabaseManagerWriteMode.jedisPipeline.set(redisPrefix + eachAuthor.id, authorJsonString);
+
+            val authorIdString = eachAuthor.id.toString;
+
+            // Exact size set
+            RedisDatabaseManagerWriteMode.jedisPipeline.sadd(AUTHORS_IDS_EXACT_SET_KEY, authorIdString);
+
+            // HyperLogLog
+            RedisDatabaseManagerWriteMode.jedisPipeline.pfadd(AUTHORS_IDS_PF_SET_KEY, authorIdString);
         });
     }
 }
