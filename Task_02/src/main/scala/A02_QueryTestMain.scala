402fe21aec0f67ff6a0614bd2e2a52ecfd8d3a0d
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-05 15:32:35 +0100

    add time logging to query tests

--- Task_02/src/main/scala/A02_QueryTestMain.scala
+++ Task_02/src/main/scala/A02_QueryTestMain.scala
@@ -1,3 +1,4 @@
+import Additional.Helpers
 import Additional.Helpers.{getTimeDifferenceStringBetween, millisecondsTimeOnStart}
 import DB_Stuff.QueryManager
 
@@ -5,17 +6,27 @@ object A02_QueryTestMain {
     def main(args: Array[String]): Unit = {
         println("Starting...");
 
+        Helpers.setInitialStartTimeMilliseconds();
+
         println("Title for article 1091:");
         println(QueryManager.titleByID(1091));
+        println(s"Elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
 
         println("Authors for article 1091:");
         println(QueryManager.authors(1091));
+        println(s"Elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
 
         println("Articles for author 2312688602:");
         println(QueryManager.articles(2312688602L));
+        println(s"Elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
 
         println("Articles referenced by article 2018037215:");
         println(QueryManager.referencedBy(2018037215L));
+        println(s"Elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
+
+        println("Exact amount of distinct authors:");
+        println(QueryManager.distinctAuthorsExact());
+        println(s"Elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
 
         println(s"Total elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
         println("Terminated.");
