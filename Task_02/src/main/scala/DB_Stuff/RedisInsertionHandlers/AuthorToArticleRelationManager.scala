eee83632dd4bf071310f057c184ea3bf027e357d
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-05 21:27:33 +0100

    add author to article amounts sorted set

--- Task_02/src/main/scala/DB_Stuff/RedisInsertionHandlers/AuthorToArticleRelationManager.scala
+++ Task_02/src/main/scala/DB_Stuff/RedisInsertionHandlers/AuthorToArticleRelationManager.scala
@@ -6,6 +6,8 @@ import JsonDefinitions.{Article, Author}
 object AuthorToArticleRelationManager extends RedisManagerTrait {
     final val redisPrefix = "r_auth-article_";
 
+    final val AUTHOR_ARTICLE_AMOUNTS_SORTED_SET_KEY = redisPrefix + "amounts";
+
     /** Add to the DB a relation from an article to multiple authors.
       *
       * @param article
@@ -21,6 +23,9 @@ object AuthorToArticleRelationManager extends RedisManagerTrait {
             val authorToArticlesRelationRedisSetName: String = redisPrefix + eachAuthor.id;
             RedisDatabaseManagerWriteMode.jedisPipeline
                 .sadd(authorToArticlesRelationRedisSetName, article.id.toString);
+
+            RedisDatabaseManagerWriteMode.jedisPipeline
+                .zincrby(AUTHOR_ARTICLE_AMOUNTS_SORTED_SET_KEY, 1, eachAuthor.id.toString);
         })
     }
 }
