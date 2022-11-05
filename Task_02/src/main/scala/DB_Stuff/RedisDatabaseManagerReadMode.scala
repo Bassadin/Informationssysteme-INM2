25c1f5f9dcdb485c37eb416ff49c698ca38e273d
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-05 15:31:15 +0100

    add default timeout of 60 secs for read mode redis connection

--- Task_02/src/main/scala/DB_Stuff/RedisDatabaseManagerReadMode.scala
+++ Task_02/src/main/scala/DB_Stuff/RedisDatabaseManagerReadMode.scala
@@ -1,12 +1,14 @@
 package DB_Stuff
 
-import redis.clients.jedis.{Jedis, JedisPool}
+import redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
 
 object RedisDatabaseManagerReadMode {
     final val DB_HOST = "localhost";
     final val DB_PORT = 32321;
+    final val JEDIS_TIMEOUT = 60 * 1000;
 
-    private val jedisConnectionPool: JedisPool = new JedisPool(DB_HOST, DB_PORT);
+    private val jedisPoolConfig: JedisPoolConfig = new JedisPoolConfig();
+    private val jedisConnectionPool: JedisPool = new JedisPool(jedisPoolConfig, DB_HOST, DB_PORT, JEDIS_TIMEOUT);
     val jedisInstance: Jedis = jedisConnectionPool.getResource;
 
     def closeConnection(): Unit = {
