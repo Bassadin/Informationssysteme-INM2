538b09c504adbc3141498b518066ae947b4436e5
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-22 10:38:29 +0100

    move spark stuff into seperate object

---
+++ Task_03/src/main/scala/Spark/SparkConnectionManager.scala
@@ -0,0 +1,28 @@
+package Spark
+
+import Additional.LoggingHelper
+import DataClasses.Article
+import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession}
+
+object SparkConnectionManager {
+    final val JSON_PATH: String = "./src/data/dblp.v12.json";
+    //    final val SPARK_MASTER_URL = "spark://localhost:7077";
+    final val SPARK_MASTER_URL = "local[*]";
+
+    // configure spark// configure spark
+    val sparkSession: SparkSession = SparkSession.builder
+        .appName("Read JSON Articles File to DataSet")
+        .master(SPARK_MASTER_URL)
+        .config("spark.sql.caseSensitive", "true")
+        .getOrCreate();
+
+    // Java Bean (data class) used to apply schema to JSON data
+    val articleEncoder: Encoder[Article] = Encoders.bean(classOf[Article]);
+
+    def readJsonFileIntoDataset(jsonPath: String = JSON_PATH): Dataset[Article] = {
+        val articlesDataset = sparkSession.read.json(jsonPath).as(articleEncoder);
+        LoggingHelper.printElapsedTimeStatusMessage();
+
+        return articlesDataset;
+    }
+}
