fc56e96d98fd747122cd79ff6c5e86fe0d605d62
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-22 10:39:14 +0100

    adjust main file to new spark conn object

--- Task_03/src/main/scala/A03_ParseJsonMain.scala
+++ Task_03/src/main/scala/A03_ParseJsonMain.scala
@@ -1,34 +1,18 @@
 import Additional.LoggingHelper
 import Additional.LoggingHelper.{getTimeDifferenceStringBetween, millisecondsTimeOnStart}
 import DataClasses.Article
+import Spark.SparkConnectionManager
 import org.apache.spark.sql.{Encoders, SparkSession}
 
 object A03_ParseJsonMain {
-    final val JSON_PATH: String = "./src/data/dblp.v12.json";
-//    final val SPARK_MASTER_URL = "spark://localhost:7077";
-    final val SPARK_MASTER_URL = "local[*]";
+
 
     def main(args: Array[String]): Unit = {
-        // Measure time before starting as reference timeframe
-
-        println(System.getProperty("java.version"));
-
+        println("Starting Task_03...");
         LoggingHelper.setInitialStartTimeMilliseconds();
 
-        println("Starting Task_03...");
 
-        // configure spark// configure spark
-        val spark = SparkSession.builder
-            .appName("Read JSON Articles File to DataSet")
-            .master(SPARK_MASTER_URL)
-            .config("spark.sql.caseSensitive", "true")
-            .getOrCreate();
-
-        // Java Bean (data class) used to apply schema to JSON data
-        val articleEncoder = Encoders.bean(classOf[Article]);
-
-        // read JSON file to Dataset
-        val jsonDataSet = spark.read.json(JSON_PATH).as(articleEncoder);
+        SparkConnectionManager.readJsonFileIntoDataset();
 
         println(s"Total elapsed time: ${getTimeDifferenceStringBetween(millisecondsTimeOnStart)}");
         println("Terminated.");
