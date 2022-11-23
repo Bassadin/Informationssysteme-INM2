896a0a3b119d856bdf9b75d7cc7abc8511b7ee15
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-23 23:10:44 +0100

    add main function to rearrange given json file to work better with spark

--- Task_03/src/main/scala/Spark/SparkConnectionManager.scala
+++ Task_03/src/main/scala/Spark/SparkConnectionManager.scala
@@ -8,7 +8,7 @@ import java.io.File
 import scala.reflect.io.Directory
 
 object SparkConnectionManager {
-    final val JSON_PATH: String = "./src/data/dblp.v12.json";
+    final val JSON_PATH: String = "./src/data/dblp.v12.new.json";
     final val PARQUET_SAVE_PATH = "src/data/articles";
 
     //    final val SPARK_MASTER_URL = "spark://localhost:7077";
