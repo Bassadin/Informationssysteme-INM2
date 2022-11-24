743bcf5bea82dde566ec4e31e23326aaf0211a9b
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-24 21:07:42 +0100

    article counts working

--- Task_03/src/main/scala/Spark/Queries/QueryManagerArticleCounts.scala
+++ Task_03/src/main/scala/Spark/Queries/QueryManagerArticleCounts.scala
@@ -1,6 +1,7 @@
 package Spark.Queries
 
 import Spark.{ParquetReader, SparkConnectionManager}
+import org.apache.spark.sql.DataFrame
 
 import scala.io.Source
 
@@ -19,5 +20,12 @@ object QueryManagerArticleCounts {
         return fileLinesAmount;
     };
 
-    def articlesCountParquet(): Long = ???;
+    def articlesCountParquet(): Long = {
+        ParquetReader.createOrReplaceArticlesView();
+
+        val sqlString = "SELECT COUNT(*) as articles_count FROM ArticlesFromJSON";
+        val sqlResultDataFrame: DataFrame = SparkConnectionManager.sparkSession.sql(sqlString);
+
+        return sqlResultDataFrame.collect()(0)(0).asInstanceOf[Long];
+    };
 }
