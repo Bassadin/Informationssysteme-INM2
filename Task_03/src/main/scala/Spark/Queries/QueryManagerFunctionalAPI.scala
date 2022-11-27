e656fce58bf6b41e9d70c45561646710e53ec114
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-27 01:41:28 +0100

    WIP most articles functional api

--- Task_03/src/main/scala/Spark/Queries/QueryManagerFunctionalAPI.scala
+++ Task_03/src/main/scala/Spark/Queries/QueryManagerFunctionalAPI.scala
@@ -1,9 +1,11 @@
 package Spark.Queries
 
+import Additional.RowConversion
 import DataClasses.Author
 import Spark.ParquetReader
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, explode}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
 
 /*
 Handles querying methods
@@ -22,8 +24,34 @@ object QueryManagerFunctionalAPI {
     };
 
     def mostArticles(): List[Author] = {
-        // TODO
-        return List.empty;
+        val authorsDataFrame: DataFrame = ParquetReader.parquetFileDataFrame
+            .select(explode(col("authors")))
+            .select("col.*");
+        authorsDataFrame.createOrReplaceTempView("authors");
+
+        val rankDataFrame = authorsDataFrame
+            .select(
+              col("id"),
+              rank()
+                  .over(Window.orderBy(count("*").desc))
+                  .as("article_count_rank")
+            )
+            .groupBy(col("id"))
+            .max();
+
+        rankDataFrame.show();
+
+        val sqlResultDataFrame = authorsDataFrame
+            .select("*")
+            .join(rankDataFrame, authorsDataFrame("id") === rankDataFrame("id"))
+            .where(rankDataFrame("article_count_rank") === 1);
+
+        val authorsList: List[Author] = sqlResultDataFrame
+            .collect()
+            .map(RowConversion.rowToAuthor)
+            .toList;
+
+        return authorsList;
     };
 
 }
