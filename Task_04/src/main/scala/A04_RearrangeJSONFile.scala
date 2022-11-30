921b50d57594c1c705659fccdfd2496f09d1f172
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-30 12:50:40 +0100

    add main to refactor json file

---
+++ Task_04/src/main/scala/A04_RearrangeJSONFile.scala
@@ -0,0 +1,38 @@
+import java.io.FileWriter
+import scala.io.Source
+import scala.util.matching.Regex
+
+object A04_RearrangeJSONFile {
+    final private val INPUT_PATH = "src/data/dblp.v12.json";
+    final private val OUTPUT_PATH = "src/data/dblp.v12.new.json";
+
+    def main(args: Array[String]): Unit = {
+        println("Starting rearranging JSON file...");
+
+        val outputFileWriter = new FileWriter(OUTPUT_PATH);
+
+        val jsonFileSource = Source.fromFile(INPUT_PATH);
+        val jsonFileLinesIterator = jsonFileSource.getLines;
+
+        // Use zipWithIndex to get an index iterator alongside the elements
+        jsonFileLinesIterator.foreach { case (eachLineString) =>
+            // Skip last line
+            val cleanedLineString = eachLineString
+                .replace("\uFFFF", "?")
+                .replaceFirst("^,", "");
+
+            val replacementRegex: Regex = """(?:"fos":\[.*?],|"indexed_abstract":\{.+?},|"venue":\{.*?},)""".r;
+
+            val rearrangedLineString = replacementRegex.replaceAllIn(cleanedLineString, "");
+
+            outputFileWriter.write(rearrangedLineString + "\n");
+
+        };
+
+        println("Closing file writer...");
+
+        outputFileWriter.close();
+
+        println("Terminated.");
+    }
+}
